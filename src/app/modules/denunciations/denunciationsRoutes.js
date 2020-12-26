5b7b52edb716ba0ad906238df00694cf40a2df19
leonardo.ferreira <leonardovff@gmail.com>
2020-12-25 23:30:33 -0300

    remove not used data anymore

--- src/app/modules/denunciations/denunciationsRoutes.js
+++ src/app/modules/denunciations/denunciationsRoutes.js
@@ -1,10 +1,12 @@
 import DenunciationsControler from './denunciationsController.js'
 
+const rootPath = '/denunciations';
+
 export default [{
-  verify: (req) => req.url == '/users' && req.method === 'GET',
+  verify: (req) => req.url == rootPath && req.method === 'GET',
   dispatch: (new DenunciationsControler).listDenunciations
 },
 {
-  verify: (req) => req.url == '/users' && req.method === 'POST',
+  verify: (req) => req.url == rootPath && req.method === 'POST',
   dispatch: (new DenunciationsControler).createDenunciations
 }]
